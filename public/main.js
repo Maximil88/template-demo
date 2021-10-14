923f6edf0a8a41ff2dd1073f924efce790cd74c1
Maximil88 <87375553+Maximil88@users.noreply.github.com>
2021-10-14 10:23:51 -0400

    update with MatD

--- public/main.js
+++
@@ -1,2 +0,0 @@
-<img src="https://upload.wikimedia.org/wikipedia/commons/8/82/William_Shatner_July_2010.jpg" 
-    width="300" height="300"></img>
--- server.js
+++ server.js
@@ -1,34 +1,41 @@
 require('dotenv').config();
+const { application } = require('express');
 const express = require('express');
 const es6Renderer = require('express-es6-template-engine');
 const morgan = require('morgan');
 
 const server = express();
 
-server.get('/heartbeat', (reg, res) => {
-    res.json({
-        "is": "working"
-    })
+server.get('/heartbeat', (req, res) => {
+  res.json({
+    "is": "working"
+  })
 });
 
 
 server.use('/', express.static(__dirname + '/public'));
+
+
 // template rendering
 server.engine('html', es6Renderer);
 server.set('views', 'views');
 server.set('view engine', 'html');
 
+/*server.get creates a route 
+renders response to landingjs*/
 server.get('/', (req, res) => {
   res.render('landing', {
     partials: {
-      footer: "partials/footer",
-      header: "partials/header"
+      footer: 'partials/footer',
+      header: 'partials/header'
     }
   });
 })
 
-const {PORT} = process.env;
+const { PORT } = process.env;
 
+// type in node server.js and it should run
+// the server listens to requests and responds
 server.listen(PORT, () => {
   console.log(`The server is running at ${PORT}`);
 })
